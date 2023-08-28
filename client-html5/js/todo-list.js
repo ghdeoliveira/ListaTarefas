4a6a20ba596296c9d4da7fa175ad26c6c611095d
Gabriel Henrique <gh.deoliveira@outlook.com>
2023-08-28 17:15:40 -0300

    save button and task complete

--- client-html5/js/todo-list.js
+++ client-html5/js/todo-list.js
@@ -1,17 +1,25 @@
 let style
+let fontAwesome
 
 function createLinkStyle() {
     if (style) return
     style = document.createElement("link")
     style.rel = "stylesheet"
     style.href = "./css/todo-list.css" // @REFATORAR
-    document.head.append(style)
+
+    if (fontAwesome) return
+    fontAwesome = document.createElement("link")
+    fontAwesome.rel = "stylesheet"
+    fontAwesome.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+
+    document.head.append(style, fontAwesome)
 }
 
 function createActionbar () {
     const actionBar = document.createElement("div")
     const input = document.createElement("input")
     const addButton = document.createElement("button")
+    input.placeholder = "Adicione uma tarefa"
     addButton.textContent = "+"
     actionBar.append(input, addButton)
     actionBar.className = "action-bar"
@@ -29,15 +37,36 @@ function createItem (labelText) {
     const checkbox = document.createElement("input")
     const btDelete = document.createElement("button")
     const btEdit = document.createElement("button")
-    const label = document.createElement("span")
+    const label = document.createElement("input")
+    const iconDelete = document.createElement("i")
+    const iconEdit = document.createElement("i")
+
     checkbox.type = "checkbox"
-    btDelete.textContent = "🗑"
+    checkbox.className = "checkbox"
+    checkbox.addEventListener("change", () => {
+        const label = item.querySelector(".item-label");
+        if (checkbox.checked) {
+            label.classList.add("completed"); 
+        } else {
+            label.classList.remove("completed");
+        }
+    });
+
+    btDelete.append(iconDelete)
+    iconDelete.className = "fa fa-trash"
+
     btEdit.className = "edit"
-    btEdit.textContent = "edit"
+    btEdit.append(iconEdit)
+    iconEdit.className = "fa fa-pencil"
+
+    label.value = labelText
     label.textContent = labelText
-    label.id = "span"
-    item.append(checkbox, label, btDelete, btEdit)
-    return { item, checkbox, label, btDelete, btEdit }
+    label.className = "item-label"
+    label.setAttribute('readonly', 'readonly')
+    label.type = 'text'
+
+    item.append(checkbox, label, btEdit, btDelete)
+    return { item, checkbox, label, btEdit, btDelete }
 }
 
 export default function(rootElement) {
@@ -56,14 +85,50 @@ export default function(rootElement) {
 
         const { item, btEdit, btDelete } = createItem(input.value)
         btDelete.addEventListener("click", () => item.remove())
-        btEdit.addEventListener("click", () => item.editItem())
+        btEdit.addEventListener("click", (e) => editItem(item, btEdit))
         input.value = ""
         list.append(item)
     }
+    /*
+    function editItem(item, editButton) {
+        const label = document.querySelector(".item-label");
+        console.log(label)
+        if (editButton.innerText.toLowerCase() == 'edit') {
+            editButton.innerText = 'save';
+            label.removeAttribute("readonly");
+            label.focus();
+        }
+        else {
+            editButton.innerText = 'edit';
+            label.setAttribute('readonly', 'readonly');
+        }
+    }
+    */
 
-    function editItem () {
-        const task = document.getElementById('span');
-        console.log(task.innerText);
+    function editItem(item, editButton) {
+        
+        const label = item.querySelector(".item-label");
+        const originalText = label.textContent;
+                
+        const editInput = document.createElement("input");
+        editInput.type = "text";
+        editInput.value = originalText;
+        label.replaceWith(editInput);
+
+        const saveButton = editButton;
+        //saveButton.innerText = "save";
+      
+        saveButton.addEventListener("click", () => {
+            const newText = editInput.value;
+            if (newText.trim() !== "") {
+                label.value = newText
+                label.textContent = newText;
+                //saveButton.innerText = "edit";
+                editButton = saveButton;
+            }
+            editInput.replaceWith(label);
+        });
+        list.removeEventListener("click", saveButton);
     }
 
     // input.addEventListener("keydown", ({ key }) => key == "Enter" && addNewItem())
